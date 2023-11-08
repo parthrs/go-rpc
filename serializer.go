94df9807184debdf4e1bd5ed0a96d0459aa31138
Parth S <parth@MacBook-Pro.attlocal.net>
2023-11-08 13:47:58 -0800

    Add de/serialize tests

--- serializer.go
+++ serializer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -12,13 +13,39 @@ import (
 func WriteProtobufToBinaryFile(message proto.Message, fileName string) error {
 	data, err := proto.Marshal(message)
 	if err != nil {
-		return fmt.Errorf("Unable to marshal proto message to binary %w", err)
+		return fmt.Errorf("unable to marshal proto message to binary %w", err)
 	}
 
 	err = os.WriteFile(fileName, data, 0644)
 	if err != nil {
-		return fmt.Errorf("Unable to write to file %w", err)
+		return fmt.Errorf("unable to write to file %w", err)
 	}
 
 	return nil
+}
+
+func ReadProtobufFromBinaryFile(filename string, message proto.Message) error {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return fmt.Errorf("unable to read from binary file %w", err)
+	}
+
+	err = proto.Unmarshal(data, message)
+	if err != nil {
+		return fmt.Errorf("unable to marshal from binary file %w", err)
+	}
+	return nil
+}
+
+func WriteProtobufToJSONFile(message proto.Message, filename string) (err error) {
+	data, err := protojson.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("unable to marshal message to json %w", err)
+	}
+
+	err = os.WriteFile(filename, data, 0644)
+	if err != nil {
+		return fmt.Errorf("unable to write to json file %w", err)
+	}
+	return
 }
--- serializer_test.go
+++ serializer_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	gen "go-rpc/gen/go-rpc"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestWriteProtobufToBinaryFile(t *testing.T) {
@@ -12,5 +14,17 @@ func TestWriteProtobufToBinaryFile(t *testing.T) {
 	binaryFile := "laptop.bin"
 	laptop1 := NewLaptop()
 	err := WriteProtobufToBinaryFile(laptop1, binaryFile)
+	require.NoError(t, err)
+
+	laptop2 := &gen.Laptop{}
+	err = ReadProtobufFromBinaryFile(binaryFile, laptop2)
+	require.NoError(t, err)
+	require.True(t, proto.Equal(laptop1, laptop2))
+}
+
+func TestWriteProtobufToJSONFile(t *testing.T) {
+	jsonFile := "laptop.json"
+	laptop1 := NewLaptop()
+	err := WriteProtobufToJSONFile(laptop1, jsonFile)
 	require.NoError(t, err)
 }
